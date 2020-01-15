b5633704d5dd9edd569183d91e1ea819eeb41e59
Roy Cao <335050309@qq.com>
2020-01-15 18:28:48 +0800

    Bump version to 0.1.0

--- Sources/Pecker/main.swift
+++ Sources/Pecker/main.swift
@@ -20,7 +20,7 @@ fileprivate func main(_ arguments: [String]) -> Int32 {
         }
         return 0
     case .version:
-        print("0.0.12")
+        print("0.1.0")
         return 0
     }
 }
