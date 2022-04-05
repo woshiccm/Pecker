f148956b9c74fba4b6b7da3529dd3f871e0b5ae1
chijie <chijie@bytedance.com>
2022-04-05 21:11:14 +0800

    feat: fix dependency error

--- Package.swift
+++ Package.swift
@@ -1,20 +1,49 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+var dependencies: [Package.Dependency] = [
+    .package(name: "IndexStoreDB", url: "https://github.com/apple/indexstore-db.git", .branch("release/5.3")),
+    .package(name: "swift-tools-support-core", url: "https://github.com/apple/swift-tools-support-core.git", .branch("main")),
+    .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.2")),
+]
+
+#if swift(>=5.5)
+dependencies.append(
+    .package(
+        name: "SwiftSyntax",
+        url: "https://github.com/apple/swift-syntax",
+        .exact("0.50500.0")
+    )
+)
+#elseif swift(>=5.4)
+dependencies.append(
+    .package(
+        name: "SwiftSyntax",
+        url: "https://github.com/apple/swift-syntax",
+        .exact("0.50400.0")
+    )
+)
+#elseif swift(>=5.3)
+dependencies.append(
+    .package(
+        name: "SwiftSyntax",
+        url: "https://github.com/apple/swift-syntax",
+        .exact("0.50300.0")
+    )
+)
+#else
+fatalError("This version of Periphery does not support Swift <= 5.2.")
+#endif
 
 let package = Package(
     name: "pecker",
     products: [
         .executable(name: "pecker", targets: ["Pecker"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
-        .package(url: "https://github.com/apple/indexstore-db.git", .branch("release/5.3")),
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("main")),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.2")),
-    ],
+    dependencies: dependencies,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
@@ -22,7 +51,7 @@ let package = Package(
             name: "Pecker",
             dependencies: [
                 "PeckerKit",
-                "SwiftToolsSupport-auto",
+                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser")]
         ),
         .target(
@@ -30,7 +59,7 @@ let package = Package(
             dependencies: [
                 "SwiftSyntax",
                 "IndexStoreDB",
-                "SwiftToolsSupport-auto",
+                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
                 "Yams"
             ]
         ),
